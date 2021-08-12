c0e8bbb0257be0eb0c41ec879abe1cc9da3ef998
Anurag Pathak <anuragpthak911@gmail.com>
2021-08-12 10:34:55 +0530

    Day1 leetcode Done

--- 1470. Shuffle the Array.cpp
+++ 1470. Shuffle the Array.cpp
@@ -20,6 +20,19 @@ public:
         {
             ans[2 * i + 1] = nums[i + n ];
         }
+
+        // alter
+        // for (int i = 0; i < 2*n; i++)
+        // {
+        //     if (i<n)
+        //     {
+        //         ans[2 * i] = nums[i];
+        //     }
+        //     else
+        //     {
+        //         ans[2 * (i-n) + 1] = nums[i];
+        //     }
+        // }
         return ans;
     }
 };
